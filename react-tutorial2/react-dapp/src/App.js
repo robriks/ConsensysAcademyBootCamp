a77d6dc2cc204ef5e399138718546994c55d737e
robriks <marsterlund@gmail.com>
2021-10-11 21:42:29 -0400

    working basic react app that sets and fetches greeting messages on a smart contract

--- react-tutorial2/react-dapp/src/App.js
+++ react-tutorial2/react-dapp/src/App.js
@@ -1,14 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { ethers } from 'ethers';
+import Greeter from './build/contracts/Greeter.json';
+
+const greeterAddress = "0xFc520f0Ba4E98536DCb6fDF32DD809F3c2765533";
 
 function App() {
+
+  const [greeting, setGreetingValue] = useState()
+
+  async function requestAccount() {
+    await window.ethereum.request({ method: 'eth_requestAccounts' });
+  }
+
+  // call smart contract, fetch current greeting
+  async function fetchGreeting() {
+    if (typeof window.ethereum !== 'undefined') {
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider)
+      try {
+        const data = await contract.greet()
+        console.log('data: ', data)
+      } catch (err) {
+        console.log("Error: ", err)
+      }
+    }
+  }
+
+  // call smart contract, send updated greeting
+  async function setGreeting() {
+    if (!greeting) return
+    if (typeof window.ethereum !== 'undefined') {
+      await requestAccount()
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner()
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer)
+      const transaction = await contract.setGreeting(greeting)
+      await transaction.wait()
+      fetchGreeting()
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +54,9 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={fetchGreeting}>Fetch Greeting</button>
+        <button onClick={setGreeting}>Set Greeting</button>
+        <input onChange={e => setGreetingValue(e.target.value)} placeholder="Set greeting" />
       </header>
     </div>
   );
